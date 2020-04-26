16770c750d0d67b58dff1d45de2188ec5d106a5a
exkuretrol <qaz855175b@gmail.com>
2020-04-26 18:02:34 +0000

    test

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -114,6 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
