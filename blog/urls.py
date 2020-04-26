0590461f321cb8780a2160c6be301bdecf8e119e
exkuretrol <qaz855175b@gmail.com>
2020-04-26 17:07:45 +0000

    Chapter 6: Form

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BlogPageView, BlogDetailView
+from .views import (
+    BlogPageView, 
+    BlogDetailView,
+    BlogCreateView, 
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 urlpatterns = [
     path('', BlogPageView.as_view(), name='home'),
-    path('posts/<int:pk>/', BlogDetailView.as_view(), name = 'post_detail')
+    path('posts/<int:pk>/edit/', BlogUpdateView.as_view(), name = 'post_edit'),
+    path('posts/<int:pk>/delete/', BlogDeleteView.as_view(), name = 'post_delete'),
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name = 'post_detail'),
+    path('posts/new/', BlogCreateView.as_view(), name='post_new')
 ]
